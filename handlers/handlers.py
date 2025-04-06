24f0a3f23aed827e21d2344a37b28b607b2478e4
@PyAdminRU <zh.vitaliy92@gmail.com>
2025-04-06 10:51:22 +0300

    Рефакторинг кода

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+import json
+import random
+
 from aiogram.filters import Command
 from aiogram.filters import CommandStart
 from aiogram.types import Message
@@ -10,51 +14,45 @@ from system.dispatcher import bot, dp
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     """Обработчик команды /start"""
-    user_id = message.from_user.id
-    user_name = message.from_user.username
-    user_first_name = message.from_user.first_name
-    user_last_name = message.from_user.last_name
-    user_date = message.date.strftime("%Y-%m-%d %H:%M:%S")
-    logger.info(f"{user_id} {user_name} {user_first_name} {user_last_name} {user_date}")
-    sign_up_text = "Добро пожаловать"
-    await bot.send_message(message.from_user.id, sign_up_text, disable_web_page_preview=True)
+    logger.info(f"{message.from_user.id} {message.from_user.username} {message.from_user.first_name} "
+                f"{message.from_user.last_name} {message.date.strftime("%Y-%m-%d %H:%M:%S")}")
+    await bot.send_message(message.from_user.id, "Добро пожаловать", disable_web_page_preview=True)
 
 
 # Функция для получения случайного комментатора поста
 async def get_random_commenter(channel_id, message_id):
     """Функция для получения случайного комментатора поста"""
-    api_id = 12345
-    api_hash = '0123456789abcdef0123456789abcdef'
     session_name = 'session_name'
-
-    async with TelegramClient(f'setting/account/{session_name}', api_id, api_hash) as client:
+    async with TelegramClient(f'setting/account/{session_name}', 12345, '0123456789abcdef0123456789abcdef') as client:
         await client.connect()
         logger.info(f'Подключено к аккаунту Telegram с именем сеанса {session_name}')
-
         # Список для хранения ID и username пользователей
         commenters = []
-
         async for message in client.iter_messages(channel_id, reply_to=message_id, reverse=True):
             sender_id = message.from_id.user_id if message.from_id else None
             username = message.sender.username if message.sender else None
             logger.info(
                 f"Получен ID пользователя: {sender_id}, Имя пользователя: @{username}")  # Получаем ID пользователя и имя пользователя
             commenters.append((sender_id, username))
-
-        import random
-        random_commenter = random.choice(commenters)
-
         await client.disconnect()  # Отключение от аккаунта Telegram
-    return random_commenter
+    return random.choice(commenters)
+
+
+async def reading_json_file():
+    """Чтение JSON файла"""
+    with open('config.json', 'r') as file:
+        data = json.load(file)
+    return data
 
 
 @dp.message(Command('random_commenter'))
 async def random_commenter_handler(message: Message):
     """Обработчик команды /random_commenter"""
-    channel_id = '@master_tg_d'  # ID вашего канала или группы
-    post_id = 492  # ID поста, к которому нужно получить комментаторов
-    random_commenter = await get_random_commenter(channel_id, post_id)
+    data = await reading_json_file()
+    channel_id = data['channel_id']
+    post_id = data['post_id']
 
+    random_commenter = await get_random_commenter(channel_id, post_id)
     if random_commenter:
         await message.reply(f"Случайный комментатор: {random_commenter}")
     else:
